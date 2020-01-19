771a149c2e7b0445fe67e4674f66a8c65d85c4de
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-01-19 17:51:44 +0530

    Create Pascal Triangle.java

---
+++ Pascal Triangle.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> solve(int A) {
+        int i,j,n,val;
+        ArrayList<ArrayList<Integer>> B=new ArrayList<ArrayList<Integer>>();
+        n=A;
+        if(n==0)
+        return B;
+        for(i=0;i<n;i++)
+        {
+           ArrayList<Integer> b=new ArrayList<Integer>();
+           for(j=0;j<=i;j++)
+           {
+               if(j==0 || i==j)
+               {
+                   b.add(1);
+               }
+               else 
+               {
+                   val=B.get(i-1).get(j-1)+B.get(i-1).get(j);
+                   b.add(val);
+               }
+           }
+           B.add(b);
+        }
+        return B;
+    }
+}
