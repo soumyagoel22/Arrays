99ac08dafaed2e0718789dd9989151d8de5f397c
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-01-21 23:29:14 +0530

    Create Find Duplicate in Array.java

---
+++ Find Duplicate in Array.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST
+    public int repeatedNumber(final List<Integer> A) {
+        Collections.sort(A);
+        int i,n,a;
+        n=A.size();
+        for(i=1;i<n;i++)
+        {
+            if((A.get(i)).equals(A.get(i-1)))
+            {
+                a=A.get(i);
+                return a;
+            }
+        }
+        return -1;
+    }
+}
