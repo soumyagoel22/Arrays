8d136d33c0894a2b4090192a21ec18ed703b704e
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-01-22 16:25:07 +0530

    Create Add One To Number.java

---
+++ Add One To Number.java
@@ -0,0 +1,33 @@
+ public class Solution {
+    public ArrayList<Integer> plusOne(ArrayList<Integer> A) {
+       int i,n;
+       n=A.size();
+       ArrayList<Integer> res = new ArrayList<Integer>(A);
+       int d,a,carry;
+       carry=1;
+       for(i=n-1;i>=0;i--)
+       {
+           if(carry==0)
+           break;
+           d=A.get(i);
+           a=d+carry;
+           if(a>=10)
+           {
+               carry=a/10;
+               d=a%10;
+           }
+           else
+           {
+               carry=0;
+               d=a;
+           }
+           res.set(i,d);
+       }
+       if(carry!=0)
+       res.add(0,1);
+       int j=0;
+       while(j<res.size() && res.get(j)==0)
+       res.remove(j);
+       return res;
+    }
+}
