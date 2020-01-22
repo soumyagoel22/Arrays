8113037ed74b3de1f513d95b58607524ecb535fc
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-01-22 16:22:43 +0530

    Create Noble Integer.java

---
+++ Noble Integer.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int solve(ArrayList<Integer> A) {
+        Collections.sort(A);
+        int n=A.size();
+        int i;
+        if(A.get(n-1)==0)
+        return 1;
+        for(i=n-1;i>=0;i--)
+        {
+            if(i<n-1 && A.get(i)<A.get(i+1)){
+            // continue;
+            if(A.get(i)==(n-i-1))
+            return 1;
+            }
+        }
+        if()
+        return -1;
+    }
+}
