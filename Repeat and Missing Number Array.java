866beaa51abaf0610bfc925e8a7c17530df7779e
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-02-07 14:46:25 +0530

    Create Repeat and Missing Number Array.java

---
+++ Repeat and Missing Number Array.java
@@ -0,0 +1,28 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public ArrayList<Integer> repeatedNumber(final List<Integer> A)
+    {  
+       ArrayList<Integer> b = new ArrayList<Integer>();
+       int i,a;
+       long tsum,sum,diff;
+       long n;
+       n=A.size();
+       tsum = (n*(n+1))/2;
+       sum=0;
+       a=0;
+       HashSet<Integer> hset = new HashSet<Integer>();
+       for(i=0;i<n;i++)
+       {
+           if(hset.contains(A.get(i)))
+           {
+               a=A.get(i);
+           }
+           hset.add(A.get(i));
+           sum+=A.get(i);
+       }
+       diff = tsum - sum;
+       b.add(a);
+       b.add(a+(int)diff);
+       return b;
+    }
+}
