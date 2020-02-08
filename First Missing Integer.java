c77d79ca66744f5a761a6f5c14ce27f42ed9de45
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-02-08 13:03:56 +0530

    Create First Missing Integer.java

---
+++ First Missing Integer.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int firstMissingPositive(ArrayList<Integer> A) {
+        int i,a,n;
+        a=1;
+        n=A.size();
+        Collections.sort(A);
+        for(i=0;i<n;i++)
+        {
+            if(A.get(i)>0)
+            {
+                if(A.get(i)==a)
+                {
+                    a++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return a;
+    }
+}
