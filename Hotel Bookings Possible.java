2c026f5357913ef7f180c30bc16572b60a3c64f2
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-02-08 13:05:43 +0530

    Create Hotel Bookings Possible.java

---
+++ Hotel Bookings Possible.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public boolean hotel(ArrayList<Integer> arrive, ArrayList<Integer> depart, int K) {
+         Collections.sort(arrive);
+         Collections.sort(depart);
+         int i,j,n,m,reqroom;
+         i=0;
+         j=0;
+         n=arrive.size();
+         m=depart.size();
+         reqroom=0;
+         while(i<n && j<m && reqroom<=K)
+         {
+             if(arrive.get(i)<depart.get(j))
+             {
+                 reqroom++;
+                 i++;
+             }
+             else
+             {
+                 reqroom--;
+                 j++;
+             }
+         }
+         if(reqroom<=K)
+         return true;
+         else
+         return false;
+    }
+}
