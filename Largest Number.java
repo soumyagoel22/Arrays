f3c8a4ba046d515b8b3ed555f87cd1f075920264
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-02-22 13:06:52 +0530

    Create Largest Number.java

---
+++ Largest Number.java
@@ -0,0 +1,26 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public String largestNumber(final List<Integer> A) {
+        ArrayList<String> str = new ArrayList<String>();
+        int i,n;
+        n=A.size();
+        for(Integer a : A)
+        {
+            str.add(String.valueOf(a));
+        }
+        Collections.sort(str,new Comparator<String>(){
+            public int compare(String b, String b1)
+            {
+                return (b1+b).compareTo(b+b1);
+            }
+        });
+        StringBuffer sb = new StringBuffer();
+        for(String s : str)
+        {
+            sb.append(s);
+        }
+        if(sb.charAt(0)=='0')
+        return String.valueOf(0);
+        return sb.toString();
+    }
+}
