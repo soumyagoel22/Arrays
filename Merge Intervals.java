9f4a24bf5c9e7ad4d7d3686e44c30083ae17f7d6
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-02-28 18:13:26 +0530

    Create Merge Intervals.java

---
+++ Merge Intervals.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+       Interval curr = newInterval;
+       int i=0;
+       while(i<intervals.size())
+       {
+           Interval in = intervals.get(i);
+           if(in.start>curr.end)
+           {
+               intervals.add(i,curr);
+               break;
+           }
+           else if(in.end<curr.start)
+           {
+               i++;
+           }
+           else
+           {
+               curr.start = Math.min(in.start,curr.start);
+               curr.end = Math.max(in.end,curr.end);
+               intervals.remove(i);
+           }
+       }
+       if(i==intervals.size())
+       {
+           intervals.add(curr);
+       }
+       return intervals;
+    }
+}
